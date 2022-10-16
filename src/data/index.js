97aa4a73e00a71a8534aef27298cacbf1f6382c2
Renata Machado <16.renata@gmail.com>
2022-10-16 12:00:21 -0300

    refactoring

---
+++ src/data/index.js
@@ -0,0 +1,372 @@
+exports.DataItems = [
+    {
+        name: "Paula Mehmetukaj",
+        country: "Albania​",
+        src: "https://s2.glbimg.com/Q4JUnqEffwE3pt13ufBee76HVXQ=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/albania_-_paula_mehmetukaj.jpg"
+    },
+    {
+        name: "Alina Luz Akselrad",
+        country: "Argentina",
+        src: "https://s2.glbimg.com/9OQjL7upjwETLz5w43__KgVAerM=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/argentina_-_alina_luz_akselrad.jpg"
+    },
+    {
+        name: "Monika Grigoryan",
+        country: "Armenia",
+        src: "https://s2.glbimg.com/_pcXhhOPftQxLqV-cG1i-EzErdE=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/armenia_-_monika_grigoryan.jpg"
+    },
+    {
+        name: "Helen Hernandez",
+        country: "Aruba",
+        src: "https://s2.glbimg.com/Y52v_Ob8BCw3aCPRTkUWBQCRuSg=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/aruba_-_helen_hernandez.jpg"
+    },
+    {
+        name: "Helen Hernandez",
+        country: "Australia",
+        src: "https://s2.glbimg.com/GK-tQJ54OUfU9KOTOUSpSIed3Yg=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/australia_-_maria_thattil.jpg"
+    },
+    {
+        name: "Shauntae-Ashleigh Miller",
+        country: "Bahamas",
+        src: "https://s2.glbimg.com/_gb1AI1oKJ5w7NAolO-cd6ZXmXA=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/bahamas_-_shauntae-ashleigh_miller.jpg"
+    },
+    {
+        name: "Hillary-Ann Williams",
+        country: "Barbados",
+        src: "https://s2.glbimg.com/ooJv5jb02sGIKdADx3W0hPqkSEg=/620x620/top/e.glbimg.com/og/ed/f/original/2021/05/03/barbados_-_hillary-ann_williams.jpg"
+    },
+    {
+        name: "Dhenia Covens",
+        country: "Belgium",
+        src: "https://s2.glbimg.com/ArUoXJ3wlp7tSF1jG87Iyy_lrsQ=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/belgica_-_dhenia_covens.jpg"
+    },
+    {
+        name: "Iris Salguero",
+        country: "Belize",
+        src: "https://s2.glbimg.com/gNDNsP0FbGtlNoFBnDhmbrqEpwY=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/belize_-_iris_salguero.jpg"
+    },
+    {
+        name: "Lenka Nemer",
+        country: "Bolivia",
+        src: "https://s2.glbimg.com/Mzmq8wjtucAOEpOev7tDIKUoR0k=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/bolivia_-_lenka_nemer.jpg"
+    },
+    {
+        name: "Julia Gama",
+        country: "Brazil",
+        src: "https://s2.glbimg.com/XCShoX0qep_Q5lAct4lm4ffJvvo=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/brasil_-_julia_gama.jpg"
+    },
+    {
+        name: "Shabree Frett",
+        country: "British Virgin Islands",
+        src: "https://s2.glbimg.com/Qlk__RKpH_Wb3wm7WYGSTMbPysw=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/brit._virgin_islands_-_shabree_frett.jpg"
+    },
+    {
+        name: "Radinela Chusheva",
+        country: "Bulgaria",
+        src: "https://s2.glbimg.com/-3O7tSiTfTVjFHVHsyEPG4y4smE=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/bulgaria_-_radinela_chusheva.jpg"
+    },
+    {
+        name: "Sarita Reth",
+        country: "Cambodia",
+        src: "https://s2.glbimg.com/jUmNpshQK8vU2p3Tt6cOiQ3tESc=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/cambodia_-_sarita_reth.jpg"
+    },
+    {
+        name: "Kossinda Angele",
+        country: "Cameroon",
+        src: "https://s2.glbimg.com/i6VJNtsaY4L4ShokJ0rdir7Luq8=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/cameroes_-_kossinda_angele.jpg"
+    },
+    {
+        name: "Nova Stevens",
+        country: "Canada",
+        src: "https://s2.glbimg.com/T__OOm-hQYa7cL6rGQWvHJ9P4eo=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/canada_-_nova_stevens.jpg"
+    },
+    {
+        name: "Mariah Tibbetts",
+        country: "Cayman Islands",
+        src: "https://s2.glbimg.com/WyGh7cjqeWQbaGOgOHcGlJqRQ3c=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/cayman_islands_-_mariah_tibbetts.jpg"
+    },
+    {
+        name: "Daniela Nicolás",
+        country: "Chile",
+        src: "https://s2.glbimg.com/7ctmnwG6aOlvBsizBvnOvYb_H-w=/620x620/e.glbimg.com/og/ed/f/original/2021/04/28/chile_-_daniela_nicolas.jpg"
+    },
+    {
+        name: "Jiaxin Sun",
+        country: "China",
+        src: "https://s2.glbimg.com/0Lu0eaxI58kRm0PXu7lj_4dCWMA=/620x620/e.glbimg.com/og/ed/f/original/2021/04/28/china_-_jiaxin_sun.jpg"
+    },
+    {
+        name: "Laura Olascuaga",
+        country: "Colombia",
+        src: "https://s2.glbimg.com/V984DepuMlQKjTQv11zmTiDjs1Q=/620x620/e.glbimg.com/og/ed/f/original/2021/04/28/colombia_-_laura_olascuaga.jpg"
+    },
+    {
+        name: "Ivonne Cerdas",
+        country: "Costa Rica",
+        src: "https://s2.glbimg.com/jUAIVTYiH7RnA0b39f_unVPeF2Q=/620x620/e.glbimg.com/og/ed/f/original/2021/04/28/costa_rica_-_ivonne_cerdas.jpg"
+    },
+    {
+        name: "Mirna Naiia Maric",
+        country: "Croatia",
+        src: "https://s2.glbimg.com/h7_41puzIETbj9xzwWjOWe7nVmU=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/croacia_-_mirna_naiia_maric.jpg"
+    },
+    {
+        name: "Chantal Wiertz",
+        country: "Curaçao",
+        src: "https://s2.glbimg.com/klBMQuPxkCQT2ulA_YLSQUe0DCw=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/curacao_-_chantal_wiertz.jpg"
+    },
+    {
+        name: "Klára Vavrušková",
+        country: "Czech Republic",
+        src: "https://s2.glbimg.com/0S2Xt4_crorLN0qqVLDUiEKfANs=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/czech_rep._-_klara_vavruskova.jpg"
+    },
+    {
+        name: "Amanda Petri",
+        country: "Denmark",
+        src: "https://s2.glbimg.com/OCm0vMYxILWytZZejzSn2Trqnmo=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/dinamarca_-_amanda_petri.jpg"
+    },
+    {
+        name: "Kimberly Jiménez",
+        country: "Dominican Republic",
+        src: "https://s2.glbimg.com/IJqcyFq315ztMhsVNa_M1rWmsRM=/620x620/e.glbimg.com/og/ed/f/original/2021/04/28/dom._rep_-_kimberly_jimenez.jpg"
+    },
+    {
+        name: "Leyla Espinoza",
+        country: "Ecuador",
+        src: "https://s2.glbimg.com/F0JxY47WOb08t5QbtPb9Jr2cIDU=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/ecuador_-_leyla_espinoza.jpg"
+    },
+    {
+        name: "Vanessa Velásquez",
+        country: "El Salvador",
+        src: "https://s2.glbimg.com/Gc3NtRFs0aCz7VEjjgqauwXTpeg=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/el_salvador_-_vanessa_velasquez.jpg"
+    },
+    {
+        name: "Viivi Altonen",
+        country: "Finland",
+        src: "https://s2.glbimg.com/PSg_zqhEWTFuSUukAl5xDNavq6I=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/finlandia_-_viivi_altonen.jpg"
+    },
+    {
+        name: "Amandine Petit",
+        country: "France",
+        src: "https://s2.glbimg.com/_uIm3Wwz_Tr6EgMkHWn4sUbVp_o=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/franca_-_amandine_petit.jpg"
+    },
+    {
+        name: "Chelsea Tayui",
+        country: "Ghana",
+        src: "https://s2.glbimg.com/PinxnU4MrxacZEbDYPOtYmFH30w=/620x620/e.glbimg.com/og/ed/f/original/2021/04/28/gana_-_chelsea_tayui.jpg"
+    },
+    {
+        name: "Jeanette Akua",
+        country: "Great Britain",
+        src: "https://s2.glbimg.com/7e5rGE1GvPrcs6RBbz0K5I_1yjM=/620x620/e.glbimg.com/og/ed/f/original/2021/04/28/gra-bretanha_-_jeanette_akua.jpg"
+    },
+    {
+        name: "Eden Berandoive",
+        country: "Haiti",
+        src: "https://s2.glbimg.com/WKQySLilEPQj2SGDJCcaQg3VXjU=/620x620/e.glbimg.com/og/ed/f/original/2021/04/28/haiti_-_eden_berandoive.jpg"
+    },
+    {
+        name: "Cecilia Rossell",
+        country: "Honduras",
+        src: "https://s2.glbimg.com/yq1LSs3FUyIPjpBuKpEXpnZ4AWE=/620x620/e.glbimg.com/og/ed/f/original/2021/04/28/honduras_-_cecilia_rossell.jpg"
+    },
+    {
+        name: "Elísabet Hulda",
+        country: "Iceland",
+        src: "https://s2.glbimg.com/L67s2UVV_GMVK4dL7DXjjEiHriE=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/iceland_-_elisabet_hulda.jpg"
+    },
+    {
+        name: "Adline Quadros Castelino",
+        country: "India",
+        src: "https://s2.glbimg.com/wtO40DFxv7kd8GTGxxbJULGpgMk=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/india_-_adline_castelino.jpg"
+    },
+    {
+        name: "Ayu Maulida Putri",
+        country: "Indonesia",
+        src: "https://s2.glbimg.com/nH1xtZkUE0aF4Vy2b7PRrtGY4r0=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/indonesia_-_ayu_maulida_putri.jpg"
+    },
+    {
+        name: "Nadia Sayers",
+        country: "Ireland",
+        src: "https://s2.glbimg.com/BWYkuFkNqynbELrK2GhtIwZQljI=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/irlanda_-_nadia_sayers.jpg"
+    },
+    {
+        name: "Tehila Levi",
+        country: "Israel",
+        src: "https://s2.glbimg.com/LxNljxRE3-CoO8S_EQr7bQsd9d4=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/israel_-_tehila_levi.jpg"
+    },
+    {
+        name: "Viviana Vizzini",
+        country: "Italy",
+        src: "https://s2.glbimg.com/sGlPQPPivVZp_SfkggRUpC-FsXE=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/italia_-_viviana_vizzini.jpg"
+    },
+    {
+        name: "Miqueal-Symone Williams",
+        country: "Jamaica",
+        src: "https://s2.glbimg.com/MiidORziBI1-U2yT3T7B0t_iXvU=/620x620/e.glbimg.com/og/ed/f/original/2021/04/28/jamaica_-_miqueal-symone_williams.jpg"
+    },
+    {
+        name: "Aisha Harumi Tochigi",
+        country: "Japan",
+        src: "https://s2.glbimg.com/uHr_OPIu6ptWg267wAgIUj9dlgk=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/japao_-_aisha_harumi_tochigi.jpg"
+    },
+    {
+        name: "Kamila Serikbai",
+        country: "Kazakhstan",
+        src: "https://s2.glbimg.com/mxsm0Rbs-ejQb2xIRI9dRjQPJvc=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/kazaquistao_-_kamila_serikbai.jpg"
+    },
+    {
+        name: "Hari Park",
+        country: "Korea",
+        src: "https://s2.glbimg.com/wg2T8gIgZRBB_rtcL6hjcVAp6NI=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/korea_do_sul_-_hari_park.jpg"
+    },
+    {
+        name: "Blerta Veseli",
+        country: "Kosovo",
+        src: "https://s2.glbimg.com/CRyCKwK3ViGrXh-PdW29Du6d7yE=/620x620/e.glbimg.com/og/ed/f/original/2021/04/28/kosovo_-_blerta_veseli.jpg"
+    },
+    {
+        name: "Christina Lasasimma",
+        country: "Laos",
+        src: "https://s2.glbimg.com/-5HjtFKP_pIb9xRebUaHpVes28U=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/laos_-_christina_lasasimma.jpg"
+    },
+    {
+        name: "Francisca Luhong James",
+        country: "Malaysia",
+        src: "https://s2.glbimg.com/oy4Y3DCJzGpDHDCOBkU3BO4P8T8=/620x620/e.glbimg.com/og/ed/f/original/2021/04/28/malasia_-_francisca_luhong_james.jpg"
+    },
+    {
+        name: "Anthea Zammit",
+        country: "Malta",
+        src: "https://s2.glbimg.com/nucUnZoLS_pvcmunPHa05VYMISE=/620x620/e.glbimg.com/og/ed/f/original/2021/04/28/malta_-_anthea_zammit.jpg"
+    },
+    {
+        name: "Vandana Jeetah",
+        country: "Mauritius",
+        src: "https://s2.glbimg.com/-i-I8Cbi-xZL_LhwKD4TMxtktAY=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/mauritius_-_vandana_jeetah.jpg"
+    },
+    {
+        name: "Andrea Meza",
+        country: "Mexico",
+        src: "https://s2.glbimg.com/hLL51BGLc5vVKaJdi2yUhS-FVV4=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/mexico_-_andrea_meza.jpg"
+    },
+    {
+        name: "Thuzar Wint Lwin",
+        country: "Myanmar",
+        src: "https://s2.glbimg.com/JhDfuQmwoQBwbhcsYmivWAaPte0=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/myanmar_-_thuzar_wint_lwin.jpg"
+    },
+    {
+        name: "Anshika Sharma",
+        country: "Nepal",
+        src: "https://s2.glbimg.com/jCtryhzx7GySuWOdczgNOxoA_d0=/620x620/top/e.glbimg.com/og/ed/f/original/2021/04/28/nepal_-_anshika_sharma.jpg"
+    },
+    {
+        name: "Denise Speelman",
+        country: "Netherlands",
+        src: "https://s2.glbimg.com/O2zZKJ-gekymxS8u-viXOOaWhA0=/620x620/e.glbimg.com/og/ed/f/original/2021/04/28/netherlands_-_denise_speelman.jpg"
+    },
+    {
+        name: "Ana Marcelo",
+        country: "Nicaragua",
+        src: "https://s2.glbimg.com/pb1zg4K4xn1GV55zHk5gJvAZZuo=/620x620/e.glbimg.com/og/ed/f/original/2021/04/28/nicaragua_-_ana_marcelo.jpg"
+    },
+    {
+        name: "Sunniva Frigstad",
+        country: "Norway",
+        src: "https://s2.glbimg.com/m0RsIpVUSepS9wkOhpEm8tV4pGw=/620x620/e.glbimg.com/og/ed/f/original/2021/04/28/noruega_-_sunniva_frigstad.jpg"
+    },
+    {
+        name: "Carmen Isabel Jaramillo",
+        country: "Panama",
+        src: "https://s2.glbimg.com/r5kXcDdfotdLxPNQWyXjuitpPxk=/620x620/e.glbimg.com/og/ed/f/original/2021/04/28/panama_-_carmem_isabel_jaramillo.jpg"
+    },
+    {
+        name: "Vanessa Castro",
+        country: "Paraguay",
+        src: "https://s2.glbimg.com/xWywQrB4aUHTPCzd7hkFXOEHb2s=/620x620/top/e.glbimg.com/og/ed/f/original/2021/05/03/paraguai_-_vanessa_castro.jpg"
+    },
+    {
+        name: "Janick Maceta",
+        country: "Peru",
+        src: "https://s2.glbimg.com/3ygI1xVS4TFmqdi_dC9Dl5ophyI=/620x620/top/e.glbimg.com/og/ed/f/original/2021/05/03/peru_-_janick_maceta.jpg"
+    },
+    {
+        name: "Rabiya Mateo",
+        country: "Philippines",
+        src: "https://s2.glbimg.com/edGLZSB-mdfGmKc6RKlOpzGyHAM=/620x620/top/e.glbimg.com/og/ed/f/original/2021/05/03/philipines_-_rabiya_mateo.jpg"
+    },
+    {
+        name: "Natalia Piguła",
+        country: "Poland",
+        src: "https://s2.glbimg.com/4FmoBqIAmh8nEWCIS92_X13PZS4=/620x620/top/e.glbimg.com/og/ed/f/original/2021/05/03/polonia_-_natalia_pigua.jpg"
+    },
+    {
+        name: "Cristiana Silva",
+        country: "Portugal",
+        src: "https://s2.glbimg.com/uU_pkZu8aDJ7yX54IqEL8xJWDoo=/620x620/top/e.glbimg.com/og/ed/f/original/2021/05/03/portugal_-_cristiana_silva.jpg"
+    },
+    {
+        name: "Estefanía Natalia Soto Torres",
+        country: "Puerto Rico",
+        src: "https://s2.glbimg.com/a6ZIIgkt22WZTm_klwb5c-sfgcM=/620x620/top/e.glbimg.com/og/ed/f/original/2021/05/03/porto_rico_-_estefania_torres.jpg"
+    },
+    {
+        name: "Bianca Tirsin",
+        country: "Romania",
+        src: "https://s2.glbimg.com/JZeSUMQzn905dTBHwjX8Lt1OI5U=/620x620/top/e.glbimg.com/og/ed/f/original/2021/05/03/romenia_-_bianca_tirsin.jpg"
+    },
+    {
+        name: "Alina Sanko",
+        country: "Russia",
+        src: "https://s2.glbimg.com/wnFf6h3eF8GO01bEWM1bYTdZaow=/620x620/top/e.glbimg.com/og/ed/f/original/2021/05/03/russia_-_alina_sanko.jpg"
+    },
+    {
+        name: "Bernadette Belle Ong",
+        country: "Singapore",
+        src: "https://s2.glbimg.com/rFopLU9OAuLnL-pSxCiZzGgSSlk=/620x620/top/e.glbimg.com/og/ed/f/original/2021/05/03/singapura_-_bernadette_belle_ong.jpg"
+    },
+    {
+        name: "Natália Hoštáková",
+        country: "Slovak Republic",
+        src: "https://s2.glbimg.com/tyq6dm4eVHYHgL19z40u_bCBdF0=/620x620/top/e.glbimg.com/og/ed/f/original/2021/05/03/slovak_-_natalia_hostakova.jpg"
+    },
+    {
+        name: "Nathasha Joubert",
+        country: "South Africa",
+        src: "https://s2.glbimg.com/P4dYWZUgiEq8kVwgvlMfUqy4hk0=/620x620/top/e.glbimg.com/og/ed/f/original/2021/05/03/south_africa_-_natasha_joubert.jpg"
+    },
+    {
+        name: "Andrea Martinez",
+        country: "Spain",
+        src: "https://s2.glbimg.com/AicgyjyExG1dpdwpVC6G4xFzrlI=/620x620/top/e.glbimg.com/og/ed/f/original/2021/05/03/spain_-_andrea_martinez.jpg"
+    },
+    {
+        name: "Amanda Obdam",
+        country: "Thailand",
+        src: "https://s2.glbimg.com/ZdR592dDY98_p3N9DvU9YvC_VbI=/620x620/top/e.glbimg.com/og/ed/f/original/2021/05/03/tailandia_-_amanda_obdam.jpg"
+    },
+    {
+        name: "Yelyzaveta Yastremska",
+        country: "Ukraine",
+        src: "https://s2.glbimg.com/NOMU7HPAXooLhfIav4UTj84xJII=/620x620/top/e.glbimg.com/og/ed/f/original/2021/05/03/ucrania_-_yelyzaveta_yastremska.jpg"
+    },
+    {
+        name: "Lola de los Santos Biccò",
+        country: "Uruguay",
+        src: "https://s2.glbimg.com/a5-tajuw787vkKtjO3DzWegi-aI=/620x620/top/e.glbimg.com/og/ed/f/original/2021/05/03/uruguai_-_lola_de_los_santos.jpg"
+    },
+    {
+        name: "Asya Branch",
+        country: "USA",
+        src: "https://s2.glbimg.com/u0lEOA2Z3Z4Fi8uzTWTGZw_Z57s=/620x620/top/e.glbimg.com/og/ed/f/original/2021/05/03/usa_-_asya_branch.jpg"
+    },
+    {
+        name: "Mariangel Villasmil",
+        country: "Venezuela",
+        src: "https://s2.glbimg.com/Je38pkLWZ5ofa68iZqZf7CmWZAo=/620x620/top/e.glbimg.com/og/ed/f/original/2021/05/03/venezuela_-_mariangel_villasmil.jpg"
+    },
+    {
+        name: "Khanh Van Nguyen Tran",
+        country: "Vietnam",
+        src: "https://s2.glbimg.com/JHjY0CBzJNpIkqtF1Un8n22UjGU=/620x620/top/e.glbimg.com/og/ed/f/original/2021/05/03/vietna_-_khanh_van_nguyen_tran.jpg"
+    }
+];
